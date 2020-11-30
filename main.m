e5a4bec88cb2fe420576e51a972f38fde2614e75
evyatar-bur <65293193+evyatar-bur@users.noreply.github.com>
2020-11-30 09:23:26 +0200

    first_set

---
+++ main.m
@@ -0,0 +1,83 @@
+clc ; clear all ; close all ;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% Simulation of the Cardiovascular System %%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Complete the code in places where the "..." string appears %%
+
+
+%% Flags:
+Plot_flag = 1; % 0 = off , 1 = on
+ 
+%% system parameters:
+% Time parameters
+HR           = 60 + IDs... ; % [BPM] % 60 + sum of last digits from all members
+dt           = 5e-4        ; % [sec]
+Heart_cycles = 20          ; % total heart cycles
+N_per_cycle      = ...     ; % number of steps per heart cycle
+  
+% Heart parameters
+V0           = 15  ; % [ml] V0 for Plv calculation
+Emax         = 2.0 ; % contractility
+N_Systole    = ... ; % number of points per ventricle Systole
+N_Diastole   = ... ; % number of points per ventricle Diastole
+E            = ... ; % Heart Elasticity: combine Systole (elasticity function) & Diastole (uniform value) 
+ 
+% Vascular constants:
+Ra = 0.1;  % arterial resistance 
+Rp = 1.0;  % peripheral resistance
+Rv = 0.01; % venous filling resistance
+Ca = 2.0;  % arterial compliance
+Cv = 300.0; % venous compliance 
+ 
+% Initiate variables:
+%Volume [ml]
+Vlv(1)  = 120;  % left ventricle
+Va(1)   = 270;  % arteries
+Vv(1)   = 2700; % veins 
+%Pressure [mmHg]
+Plv(1)  = 0;    % left ventricle
+Pa(1)   = 70;   % arterial capacitor
+Pv(1)   = 9;    % venous filling 
+Pao(1)  = 100;   % aorta
+%Flow [ml/sec]
+Qlv(1)  = 0;    % left ventricle (outflow)
+Qp(1)   = 0;    % peripheral resistance
+Qv(1)   = 0;    % ventricle filling (inflow)
+ 
+%% Main Program
+for CycleIdx = 1 : Heart_cycles % main loop for each heart cycle
+    for StepInCycle = 2 : N_per_cycle 
+	%calculating all variables for each cycle at N points:
+        %Volumes [ml] ...
+		...
+        %Pressures [mmHg] ...
+        ...
+        %Flows [ml/sec] ...
+        ...
+    end
+        % Save each variable from the current cycle to a continuos variable:
+        ...
+        %VolumeS [ml] ...
+		...
+        %PressureS [mmHg] ...
+        ...
+        %FlowS [ml/sec] ...
+        ...
+
+
+    % Update the initial variables before the next cycle:  ...
+	  %Volume [ml] ...
+      ...
+      %Pressure [mmHg] ...
+      ...
+      %Flow [ml/sec] ...
+	  ...
+
+end
+ 
+%% Plot
+if Plot_flag  
+    ... ; % Plotting commands:
+end
